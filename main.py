e84204192e8012705e87ef3db95d451cd7b7bb36
red5191 <the_red51@outlook.com>
2024-10-30 02:43:30 +0300

    1223

---
+++ main.py
@@ -0,0 +1,72 @@
+library = {
+    'Dune' : {'author' : 'Franklin Patrick Herbert', 'year' : 1965, 'availability' : True},
+    'Kolobok' : {'author' : 'Tolstoy Alexey Nikolaevich', 'year' : 1941, 'availability' : False},
+    'Wiedźmin' : {'author' : 'Andrzej Sapkowski', 'year' : 1986, 'availability' : True}
+}
+
+def book_list_view(library):
+    if not library:
+        book_list = 'Библиотека пуста'
+    else:
+        book_list = list(library.keys())
+    return book_list
+
+
+#dz2
+# print(book_list_view(library))
+
+
+# #dz3
+# correct = ['да', 'нет']
+# def add_book():
+#     title = input('Укажите название ')
+#     author = input('Укажите автора ')
+#     year = input('Укажите год ')
+#     if title in library:
+#         refresh = input('Обновить информацию? ').lower()
+#         while refresh not in correct:
+#             print('Пожалуйста используйте для ответа \"да\" или \"нет\"')
+#             refresh = input('Обновить информацию? ').lower()
+#         if refresh == 'да':
+#             library[title] = {'author' : author, 'year' : year, 'availability' : None}
+#             print(f'Данные по книге {title} успешно обновлены')
+#     else:
+#         library[title] = {'author' : author, 'year' : year, 'availability' : None}
+#         print(f'Данные по книге {title} успешно добавлены')
+
+
+# add_book()
+
+#dz4
+# def remove_book():
+#     title = input('Укажите название удаляемой книги ')
+#     if title in library:
+#         library.pop(title)
+#         print(f'Книга {title} успешно удалена')
+#     else:
+#         print('Такой книги в библиотеке не числится')
+#
+# remove_book()
+
+#dz5
+# def issue_book():
+#     title = input('Какую книгу выдать? ')
+#     if title in library:
+#         book = library[title]
+#         book['availability'] = False
+#         print(f'Книга {title} выдана')
+#     else:
+#         print('Такой книги в библиотеке не числится')
+#
+# def return_book():
+#     title = input('Какую книгу возвращают? ')
+#     if title in library:
+#         book = library[title]
+#         book['availability'] = True
+#         print(f'Книга {title} возвращена')
+#     else:
+#         print('Такой книги в библиотеке не числится')
+#
+#
+# issue_book()
+# return_book()
