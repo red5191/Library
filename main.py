4eea5df5b930091d38a900a6e13884a80755bc51
red5191 <the_red51@outlook.com>
2024-10-31 16:18:59 +0300

    123

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ library = {
     'Wiedzmin': {'author': 'Andrzej Sapkowski', 'year': 1986, 'availability': None}
 }
 
+correct = ['да', 'нет']
 
 def book_list_view(library):
     if not library:
@@ -13,4 +14,24 @@ def book_list_view(library):
     return book_list
 
 
-print(book_list_view(library))
+def add_book():
+    print('Добавляем книгу')
+    title = input('Укажите название ')
+    author = input('Укажите автора ')
+    year = input('Укажите год ')
+    if title in library:
+        refresh = input('Обновить информацию? ').lower()
+        while refresh not in correct:
+            print('Пожалуйста используйте для ответа \"да\" или \"нет\"')
+            refresh = input('Обновить информацию? ').lower()
+        if refresh == 'да':
+            library[title] = {'author' : author, 'year' : year, 'availability' : None}
+            print(f'Данные по книге {title} успешно обновлены')
+    else:
+        library[title] = {'author' : author, 'year' : year, 'availability' : None}
+        print(f'Данные по книге {title} успешно добавлены')
+
+
+print(f'В библиотеке числятся следующие наименования: {book_list_view(library)}')
+
+add_book()
